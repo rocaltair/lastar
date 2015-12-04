52b43e137a4947aedfa7d88f9ce75f2a37f15a30
rocaltair <rocaltair@gmail.com>
2015-12-04 14:26:33 +0800

    bug fix

--- test.lua
+++ test.lua
@@ -11,12 +11,11 @@ local blockmap = {
 local from = {x=1, y=1}
 local to = {x=1, y=5}
 
-local function neighbor(fx, fy, tx, ty)
+local function neighbor(w, h, fx, fy, tx, ty)
 	local idx = tx + (ty - 1) * w
 	if blockmap[idx] ~= 0 then
 		return
 	end
-
 	local dis = (tx - fx) * (ty - fy) > 0 and 1 or 1.4
 	return true, dis
 end
@@ -25,9 +24,7 @@ local map = l.new(5, 5, neighbor)
 
 local path, cost = map:path(from.x, from.y, to.x, to.y)
 
---[[
+print("cost", cost)
 table.foreach(path, function(i, v)
-	print(v[1], v[2])
+	print(string.format("path[%d] = (%d, %d)", i, v[1], v[2]))
 end)
-print(cost)
---]]
